f965ff85fcbd3ac84c1daf8b384ae827deb07352
정하 이 <ljhwsad200485@gmail.com>
2026-03-08 21:27:33 +0900

    과제 1단계 완료

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -43,20 +47,70 @@
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 25.875F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(518, 127);
+            label1.Name = "label1";
+            label1.Size = new Size(246, 92);
+            label1.TabIndex = 1;
+            label1.Text = "이정하";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label2.Location = new Point(518, 272);
+            label2.Name = "label2";
+            label2.Size = new Size(597, 65);
+            label2.TabIndex = 2;
+            label2.Text = "수원대학교 컴퓨터SW학과";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 16.125F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label3.Location = new Point(518, 359);
+            label3.Name = "label3";
+            label3.Size = new Size(558, 59);
+            label3.TabIndex = 3;
+            label3.Text = "ljhwsad200485@gmail.com";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button1.Location = new Point(862, 511);
+            button1.Name = "button1";
+            button1.Size = new Size(253, 117);
+            button1.TabIndex = 4;
+            button1.Text = "배경색 랜덤 변경";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1199, 764);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
     }
 }
