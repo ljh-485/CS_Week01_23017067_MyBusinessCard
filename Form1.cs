804bf4b850c81092f362735595699d17d9b63dd1
정하 이 <ljhwsad200485@gmail.com>
2026-03-09 22:00:14 +0900

    과제 3-2까지 완료함

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ namespace MyBusinessCard
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
